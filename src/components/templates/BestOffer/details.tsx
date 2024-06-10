396470ac47a27c2dcefbf9f47655e045c7691f34
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-06-10 18:33:06 +0330

    add new component for best offer page

---
+++ src/components/templates/BestOffer/details.tsx
@@ -0,0 +1,21 @@
+import { ProductsType } from '@/components/Type/Products.type'
+import OfferBox from '@/components/modules/OfferBox/OfferBox'
+import React from 'react'
+
+type DetailsProps={
+    products:ProductsType[]
+}
+
+function Details({products}:DetailsProps) {
+  return (
+    <div className='container flex flex-wrap gap-x-1 gap-y-3 xs:gap-4 justify-center mb-6'>
+        {
+            products.map(product=>(
+                <OfferBox {...product}/>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Details
