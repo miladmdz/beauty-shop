8716065fedb0862b83d530287d97362938127d8c
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-06-14 11:44:43 +0330

    add new page login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/modules/Footer/Footer";
+import NavBar from "@/components/modules/NavBar/NavBar";
+import Details from "@/components/templates/Login/details";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <NavBar />
+      <Details />
+      <Footer />
+    </div>
+  );
+}
+
+export default page;
