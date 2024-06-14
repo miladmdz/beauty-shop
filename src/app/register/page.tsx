6fd2b017ba8ecec6b30b27d72c8e7bcaf2d29e33
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-06-14 11:45:07 +0330

    add new page register

---
+++ src/app/register/page.tsx
@@ -0,0 +1,16 @@
+import Footer from "@/components/modules/Footer/Footer";
+import NavBar from "@/components/modules/NavBar/NavBar";
+import Form from "@/components/templates/Register/form";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <NavBar />
+      <Form />
+      <Footer />
+    </div>
+  );
+}
+
+export default page;
