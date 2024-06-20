8b794a44b275d2239acd8670472adce544830996
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-06-20 17:02:30 +0330

    update favorite page of panel user

--- src/app/p-user/favorites/page.tsx
+++ src/app/p-user/favorites/page.tsx
@@ -1,16 +1,21 @@
-import UserPanelLayout from '@/components/layouts/userPanelLayout'
-import NavBar from '@/components/modules/NavBar/NavBar'
-import React from 'react'
+import UserPanelLayout from "@/components/layouts/userPanelLayout";
+import Footer from "@/components/modules/Footer/Footer";
+import NavBar from "@/components/modules/NavBar/NavBar";
+import Details from "@/components/templates/P-User/Favorites/details";
+import React from "react";
 
 function page() {
   return (
     <div>
-      <NavBar/>
-    <UserPanelLayout>
-      <div></div>
-    </UserPanelLayout>
-  </div>
-  )
+      <NavBar />
+      <UserPanelLayout>
+        <div className="container">
+          <Details />
+        </div>
+      </UserPanelLayout>
+      <Footer />
+    </div>
+  );
 }
 
-export default page
+export default page;
