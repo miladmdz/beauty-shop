22499610e4c351bf643733305ab9ce0a8f5b0d35
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-06-27 20:43:56 +0330

    update page for handle error validation inputs

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,8 +2,10 @@ import Footer from "@/components/modules/Footer/Footer";
 import NavBar from "@/components/modules/NavBar/NavBar";
 import Form from "@/components/templates/Register/form";
 import React from "react";
+import connectToDB from "../../../configs/db";
 
 function page() {
+  connectToDB();
   return (
     <div>
       <NavBar />
