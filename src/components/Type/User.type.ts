3f71b77e8f441ec5126a1bc00bc4902ddf70a0ea
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-03 16:52:48 +0330

    add new type for users

---
+++ src/components/Type/User.type.ts
@@ -0,0 +1,15 @@
+export type UserType = {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  gender: string;
+  year: number;
+  month: number;
+  phone: number;
+  email: string;
+  password: string;
+  role: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+};
