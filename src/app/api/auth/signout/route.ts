65fbb406d0bd90e5e9fb1dfea9f82ee0a789e9ec
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-03 16:47:34 +0330

    update api and fix bugs

--- src/app/api/auth/signout/route.ts
+++ src/app/api/auth/signout/route.ts
@@ -1,7 +1,19 @@
+import connectToDB from "@/configs/db";
 import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  cookies().delete("token");
-  return Response.json({ message: "logged out successfully :)) " });
+  try {
+    connectToDB();
+    cookies().delete("token");
+    return Response.json(
+      { message: "logged out successfully :)) " },
+      { status: 201 }
+    );
+  } catch (error) {
+    return Response.json(
+      { message: `error from server is ${error}` },
+      { status: 500 }
+    );
+  }
 }
