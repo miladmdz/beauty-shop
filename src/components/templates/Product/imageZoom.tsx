5dde95be52d4eff5087801e2217029aa886d5484
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-05 12:57:23 +0330

    update to dynamic page

--- src/components/templates/Product/imageZoom.tsx
+++ src/components/templates/Product/imageZoom.tsx
@@ -3,7 +3,12 @@ import React from "react";
 import { BsArrowsFullscreen } from "react-icons/bs";
 import { CiHeart, CiShare2 } from "react-icons/ci";
 
-function ImageZoom() {
+type ImageZoomProps = {
+  img: string;
+};
+
+function ImageZoom({ img }: ImageZoomProps) {
+  console.log(img);
   return (
     <div className="flex w-full flex-col justify-start">
       <div className="flex items-center justify-center gap-x-7 child:cursor-pointer child:text-xl">
@@ -12,23 +17,10 @@ function ImageZoom() {
         <CiHeart className="md:hidden" />
       </div>
       <div>
-        <img
-          className="md:w-[300px] md:h-[300px]"
-          src="../images/Products/Capture1-1.jpg"
-          alt=""
-        />
+        <img className="md:w-[300px] md:h-[300px]" src={`${img}`} alt="" />
       </div>
       <div className="flex items-center justify-start gap-x-7">
-        <img
-          className="w-[100px] h-[100px]"
-          src="../images/Products/Capture1-1.jpg"
-          alt=""
-        />
-        <img
-          className="w-[100px] h-[100px]"
-          src="../images/Products/Capture1-2.jpg"
-          alt=""
-        />
+        <img className="w-[100px] h-[100px]" src={`${img}`} alt="" />
       </div>
     </div>
   );
