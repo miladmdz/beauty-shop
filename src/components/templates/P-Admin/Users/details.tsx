f73429d9d24769f5b8b22a067e8143699296ece2
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-07 12:23:16 +0330

    update for connect to db and active logic

--- src/components/templates/P-Admin/Users/details.tsx
+++ src/components/templates/P-Admin/Users/details.tsx
@@ -1,7 +1,126 @@
+"use client";
 import PageTitle from "@/components/modules/BreadCrumbs/pageTitle";
+import { UserType } from "@/components/Type/User.type";
+import { role } from "@/utils/constant";
+import { useRouter } from "next/navigation";
 import React from "react";
+import Swal from "sweetalert2";
 
-function Details() {
+type DetailsProps = {
+  users: UserType[];
+};
+
+function Details({ users }: DetailsProps) {
+  const router = useRouter();
+
+  const editUser = async () => {};
+
+  const changeRole = async (id: string) => {
+    Swal.fire({
+      icon: "question",
+      title: "از تغییر نقش کاربر مطمین هستید؟؟",
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: "بله",
+      denyButtonText: "خیر",
+    }).then(async (Result) => {
+      if (Result.isConfirmed) {
+        const res = await fetch("/api/user/role", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id }),
+        });
+        if (res.status === 201) {
+          router.refresh();
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "مشکل از سمت سرور",
+            showConfirmButton: true,
+            confirmButtonText: "تلاش مجدد",
+          });
+        }
+      }
+    });
+  };
+
+  const deleteUser = async (id: string) => {
+    Swal.fire({
+      icon: "question",
+      title: "از حذف کاربر مطمین هستید؟؟",
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: "بله",
+      denyButtonText: "خیر",
+    }).then(async (Result) => {
+      if (Result.isConfirmed) {
+        const res = await fetch("/api/user/delete", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id }),
+        });
+        if (res.status === 201) {
+          Swal.fire({
+            icon: "success",
+            title: "کاربر با موفقیت حذف شد",
+            showConfirmButton: true,
+            confirmButtonText: "فهمیدم",
+          }).then((result) => {
+            router.refresh();
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "مشکل از سمت سرور",
+            showConfirmButton: true,
+            confirmButtonText: "تلاش مجدد",
+          });
+        }
+      }
+    });
+  };
+
+  const banUser = async (email: string, phone: number) => {
+    Swal.fire({
+      icon: "question",
+      title: "از تغییر نقش کاربر مطمین هستید؟؟",
+      showConfirmButton: true,
+      showDenyButton: true,
+      confirmButtonText: "بله",
+      denyButtonText: "خیر",
+    }).then(async (Result) => {
+      if (Result.isConfirmed) {
+        const res = await fetch("/api/user/ban", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, phone }),
+        });
+        if (res.status === 201) {
+          Swal.fire({
+            icon: "success",
+            title: "کاربر با موفقیت بن شد",
+            showConfirmButton: true,
+            confirmButtonText: "متوجه شدم",
+          }).then((result) => {
+            router.refresh();
+          });
+        } else {
+          Swal.fire({
+            icon: "error",
+            title: "مشکل از سمت سرور",
+            showConfirmButton: true,
+            confirmButtonText: "تلاش مجدد",
+          });
+        }
+      }
+    });
+  };
   return (
     <div>
       <PageTitle title="لیست کاربران" />
@@ -21,32 +140,51 @@ function Details() {
             </tr>
           </thead>
           <tbody className="w-full">
-            <tr className="w-full flex font-bold items-center justify-between py-5 child:w-full child:flex child:justify-center">
-              <td>1</td>
-              <td className="line-clamp-1 w-[100px] text-sm">میلاد</td>
-              <td>milad@gmail.com</td>
-              <td>مدیر</td>
-              <td>
-                <button className="w-full bg-black text-white rounded-md">
-                  ویرایش
-                </button>
-              </td>
-              <td>
-                <button className="w-full bg-black text-white rounded-md">
-                  تغییر نقش
-                </button>
-              </td>
-              <td>
-                <button className="w-full bg-primryCream text-white rounded-md">
-                  حذف
-                </button>
-              </td>
-              <td>
-                <button className="w-full bg-primryCream text-white rounded-md">
-                  بن
-                </button>
-              </td>
-            </tr>
+            {users.map((user, index) => (
+              <tr
+                key={user._id}
+                className="w-full flex font-bold items-center justify-between py-5 child:w-full child:flex child:justify-center"
+              >
+                <td>{index + 1}</td>
+                <td className="line-clamp-1 w-[100px] text-sm">
+                  {user.firstName}-{user.lastName}
+                </td>
+                <td>{user.email}</td>
+                <td>{user.role === role.ADMIN ? "مدیر" : "کاربر"}</td>
+                <td>
+                  <button
+                    onClick={editUser}
+                    className="w-full bg-black text-white rounded-md"
+                  >
+                    ویرایش
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => changeRole(user._id)}
+                    className="w-full bg-black text-white rounded-md"
+                  >
+                    تغییر نقش
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => deleteUser(user._id)}
+                    className="w-full bg-primryCream text-white rounded-md"
+                  >
+                    حذف
+                  </button>
+                </td>
+                <td>
+                  <button
+                    onClick={() => banUser(user.email, user.phone)}
+                    className="w-full bg-primryCream text-white rounded-md"
+                  >
+                    بن
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
