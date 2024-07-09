55f16038a8cd8a3e660d0d675d325618dc1ab9a0
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-09 16:13:13 +0330

    add new type for comment data

---
+++ src/components/Type/Comment.type.ts
@@ -0,0 +1,11 @@
+export type CommentType = {
+  auther: string;
+  username: string;
+  createdAt: string;
+  isAccept: boolean;
+  product: string;
+  text: string;
+  score: number;
+  updatedAt: string;
+  _id: string;
+};
