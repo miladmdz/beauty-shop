321bb4ea27a7707bef5240b38119ba9fb2197590
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-09 16:11:27 +0330

    connect to db

--- src/app/p-admin/comments/page.tsx
+++ src/app/p-admin/comments/page.tsx
@@ -2,20 +2,25 @@ import AdminPanelLayout from "@/components/layouts/adminPanelLayout";
 import Footer from "@/components/modules/Footer/Footer";
 import NavBar from "@/components/modules/NavBar/NavBar";
 import Details from "@/components/templates/P-Admin/Comments/details";
+import connectToDB from "@/configs/db";
+import commentModel from "@/model/Comment";
 import React from "react";
 
-function page() {
+const page = async () => {
+  connectToDB();
+  const comments = await commentModel.find({}).populate("auther","firstName lastName phone email").lean();
+
   return (
     <>
       <NavBar />
       <AdminPanelLayout>
         <div className="container">
-          <Details/>
+          <Details comments={JSON.parse(JSON.stringify(comments))} />
         </div>
       </AdminPanelLayout>
       <Footer />
     </>
   );
-}
+};
 
 export default page;
