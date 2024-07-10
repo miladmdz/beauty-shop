1879435c0c7f584154ea89de6126642639eeb759
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-10 16:53:03 +0330

    update page and connect to db

--- src/app/p-user/tickets/send-tickets/page.tsx
+++ src/app/p-user/tickets/send-tickets/page.tsx
@@ -2,20 +2,22 @@ import UserPanelLayout from "@/components/layouts/userPanelLayout";
 import Footer from "@/components/modules/Footer/Footer";
 import NavBar from "@/components/modules/NavBar/NavBar";
 import Details from "@/components/templates/P-User/Tickets/SendTickets/details";
+import { authUser } from "@/utils/serverHelper";
 import React from "react";
 
-function page() {
+const page = async () => {
+  const user = await authUser();
   return (
     <div>
       <NavBar />
       <UserPanelLayout>
         <div className="container">
-          <Details />
+          <Details user={JSON.parse(JSON.stringify(user))} />
         </div>
       </UserPanelLayout>
       <Footer />
     </div>
   );
-}
+};
 
 export default page;
