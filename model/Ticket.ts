8979e53b987b9d8c5a8539303fde3d7edd93ec9c
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-11 10:32:53 +0330

    add new model for tickets

---
+++ model/Ticket.ts
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+require("./User");
+require("./Departmentticket");
+require("./Subdepartmentticket");
+
+const schema = new mongoose.Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+    },
+    body: {
+      required: true,
+      type: String,
+    },
+    img: {
+      required: false,
+      type: String,
+    },
+    user: {
+      required: true,
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
+    department: {
+      required: true,
+      type: mongoose.Types.ObjectId,
+      ref: "Departmentticket",
+    },
+    subDepartment: {
+      required: true,
+      type: mongoose.Types.ObjectId,
+      ref: "Subdepartmentticket",
+    },
+    priority: {
+      required: true,
+      type: Number,
+    },
+    hasAnswer: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+    isAnswer: {
+      required: false,
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ticketModel = mongoose.models.Ticket || mongoose.model("Ticket", schema);
+export default ticketModel;
