abbdaaa8247282bba841021a5b1b73e45b938dd3
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-11 10:34:59 +0330

    update component to dynamic and connect to db

--- src/components/templates/P-User/Tickets/SendTickets/details.tsx
+++ src/components/templates/P-User/Tickets/SendTickets/details.tsx
@@ -1,14 +1,18 @@
 "use client";
 import PageTitle from "@/components/modules/BreadCrumbs/pageTitle";
 import { UserType } from "@/components/Type/User.type";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import { IoIosSend } from "react-icons/io";
+import Swal from "sweetalert2";
 
 type DetailsProps = {
   user: UserType;
 };
 
 function Details({ user }: DetailsProps) {
+  const router = useRouter();
+
   const [departments, setDepartments] = useState<
     { title: string; _id: string }[]
   >([]);
@@ -20,8 +24,9 @@ function Details({ user }: DetailsProps) {
   const [selectSubDeaprtments, setSelectSubDepartments] = useState<string>("");
 
   const [titleTicket, setTitleTicket] = useState<string>("");
-  const [piority, setPiorirty] = useState<string>("");
+  const [priority, setPriority] = useState<number>(-1);
   const [textTicket, setTextTicket] = useState<string>("");
+  const [img, setImg] = useState<any>("");
 
   useEffect(() => {
     const getDepartments = async () => {
@@ -46,14 +51,76 @@ function Details({ user }: DetailsProps) {
   const sentTicket = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
-    let newTicket = {
-      department: selectDeaprtments,
-      subDepartment: selectSubDeaprtments,
-      user: user._id,
-      title: titleTicket,
-      body: textTicket,
-      piority,
-    };
+    e.preventDefault();
+
+    //validation
+
+    if (
+      !selectDeaprtments ||
+      !selectSubDeaprtments ||
+      !titleTicket.trim().length ||
+      !textTicket.trim().length
+    ) {
+      return Swal.fire({
+        icon: "error",
+        title: "لطفا تمامی کادر هارا پر کنید",
+        showConfirmButton: true,
+        confirmButtonText: "متوجه شدم",
+      });
+    } else if (!Boolean(priority < 4 && priority > 0)) {
+      return Swal.fire({
+        icon: "error",
+        title: "لطفا سطح اولویت را مشخص کنید",
+        showConfirmButton: true,
+        confirmButtonText: "متوجه شدم",
+      });
+    }
+
+    const formData = new FormData();
+
+    formData.append("department", selectDeaprtments);
+    formData.append("subDepartment", selectSubDeaprtments);
+    formData.append("user", user._id);
+    formData.append("title", titleTicket);
+    formData.append("body", textTicket);
+    formData.append("priority", String(priority));
+    formData.append("img", img);
+
+    const res = await fetch("/api/ticket", {
+      method: "POST",
+      body: formData,
+    });
+    if (res.status === 201) {
+      Swal.fire({
+        icon: "success",
+        title: "تیکت با موفقیت ارسال شد",
+        showConfirmButton: true,
+        confirmButtonText: "متوجه شدم",
+      }).then((result) => {
+        setSubDepartments([]);
+        setSelectDepartments("");
+        setSelectSubDepartments("");
+        setTitleTicket("");
+        setPriority(-1);
+        setTextTicket("");
+        setImg("");
+        router.refresh();
+      });
+    } else if (res.status === 422) {
+      Swal.fire({
+        icon: "error",
+        title: "اطلاعات اشتباه وارد شد",
+        showConfirmButton: true,
+        confirmButtonText: "تلاش مجدد",
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "مشکل از سمت سرور ",
+        showConfirmButton: true,
+        confirmButtonText: "تلاش مجدد",
+      });
+    }
   };
   return (
     <div>
@@ -130,19 +197,21 @@ function Details({ user }: DetailsProps) {
               سطح اولویت تیکت را انتخاب کنید:
             </label>
             <select
-              onChange={(e) => setPiorirty(e.target.value)}
+              onChange={(e) => setPriority(Number(e.target.value))}
               name=""
               className="w-full outline-none border-2 border-primryCream2 rounded-md px-1 py-1"
               id="important"
             >
-              <option value="-1">لطفا یک مورد را انتخاب کنید</option>
-              <option value="3">بالا</option>
-              <option value="2">متوسط</option>
-              <option value="1">پایین</option>
+              <option value={-1}>لطفا یک مورد را انتخاب کنید</option>
+              <option value={3}>بالا</option>
+              <option value={2}>متوسط</option>
+              <option value={1}>پایین</option>
             </select>
           </div>
         </div>
+
         {/* text ticket */}
+
         <div className="w-full h-64 flex flex-col items-start gap-y-2">
           <label className="font-bold pt-4" htmlFor="text-ticket">
             محتوای تیکت را وارد نمایید:
@@ -156,10 +225,18 @@ function Details({ user }: DetailsProps) {
           ></textarea>
         </div>
 
+        {/* image file */}
+
         <div className="w-full flex flex-col gap-y-2 items-center bg-primryCream3 rounded-md mt-5 py-4">
           <p>حداکثر اندازه: 6 مگابایت</p>
           <p>فرمت‌های مجاز: jpg, png.jpeg, rar, zip</p>
-          <input className="text-sm" type="file" name="" id="" />
+          <input
+            onChange={(e) => setImg(e.target.files?.[0])}
+            className="text-sm"
+            type="file"
+            name=""
+            id=""
+          />
         </div>
 
         <button
