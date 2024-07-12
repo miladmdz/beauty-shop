9933ed6e960924c7cec46b303cfb40d9bd6ce220
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-12 19:02:12 +0330

    update page to dynamic page

--- src/components/templates/P-Admin/Offer/offersList.tsx
+++ src/components/templates/P-Admin/Offer/offersList.tsx
@@ -1,7 +1,13 @@
 import PageTitle from "@/components/modules/BreadCrumbs/pageTitle";
+import { OfferType } from "@/components/Type/Offers.type";
 import React from "react";
 
-function OffersList() {
+type OffersListProps = {
+  offers: OfferType[];
+};
+
+function OffersList({ offers }: OffersListProps) {
+  console.log(offers);
   return (
     <div className="mt-10">
       <PageTitle title="لیست کد های تخفیف" />
@@ -20,19 +26,26 @@ function OffersList() {
             </tr>
           </thead>
           <tbody className="w-full">
-            <tr className="w-full flex font-bold items-center justify-between py-5 child:w-full child:flex child:justify-center">
-              <td className="bg-primryGreen2 py-2 text-white">1</td>
-              <td className="line-clamp-1 w-[100px] text-sm">test-code</td>
-              <td>50</td>
-              <td>1</td>
-              <td>0</td>
-              <td>milad</td>
-              <td>
-                <button className="w-full bg-primryCream text-white rounded-md">
-                  حذف
-                </button>
-              </td>
-            </tr>
+            {offers.map((item, index) => (
+              <tr
+                key={item._id}
+                className="w-full flex font-bold items-center justify-between py-5 child:w-full child:flex child:justify-center"
+              >
+                <td className="bg-primryGreen2 py-2 text-white">{index + 1}</td>
+                <td className="line-clamp-1 w-[100px] text-sm">
+                  {item.codeOffer}
+                </td>
+                <td>{item.percent}</td>
+                <td>{item.maxUse}</td>
+                <td>{item.uses}</td>
+                <td>{item.user.firstName}</td>
+                <td>
+                  <button className="w-full bg-primryCream text-white rounded-md">
+                    حذف
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
