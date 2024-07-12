e52a0ae61e62345b96812d46224158004f927efc
milad mahmoudzadeh <miladmahmoudzadeh5@gmail.com>
2024-07-12 18:59:38 +0330

    update page and filter on data

--- src/app/p-admin/tickets/page.tsx
+++ src/app/p-admin/tickets/page.tsx
@@ -2,20 +2,29 @@ import AdminPanelLayout from "@/components/layouts/adminPanelLayout";
 import Footer from "@/components/modules/Footer/Footer";
 import NavBar from "@/components/modules/NavBar/NavBar";
 import Details from "@/components/templates/P-Admin/Tickets/details";
+import connectToDB from "@/configs/db";
+import ticketModel from "@/model/Ticket";
 import React from "react";
 
-function page() {
+const page = async () => {
+  connectToDB();
+  const Tickets = await ticketModel
+    .find({ isAnswer: false })
+    .sort({ _id: -1 })
+    .populate("user", "_id firstName email phone")
+    .populate("department")
+    .lean();
   return (
     <>
       <NavBar />
       <AdminPanelLayout>
         <div className="container">
-          <Details />
+          <Details Tickets={JSON.parse(JSON.stringify(Tickets))} />
         </div>
       </AdminPanelLayout>
       <Footer />
     </>
   );
-}
+};
 
 export default page;
